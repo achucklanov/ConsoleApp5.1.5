a921d16839c2565dfac6b1b45b60f5fd39bbed9e
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-17 21:37:26 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5.1.5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Drawing;
+
+namespace ConsoleApp5._1._5
+{
+    internal class Program
+    {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            var favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor();
+            }
+
+            Console.WriteLine("Ваши любимы цвета: ");
+
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
+            Console.ReadKey();
+        }
+    }
+}
